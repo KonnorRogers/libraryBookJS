502ffd483de170a397f0c87d7817dfde0adbe376
paramagicdev <konnor5456@gmail.com>
2019-10-01 19:31:48 -0400

    working on localstorage

--- src/js/library.js
+++ src/js/library.js
@@ -1,16 +1,20 @@
 import Book from './book.js';
 
 export default function Library() {
-  this.books = [];
+  // Attempt to fetch local storage array of books first, if not, then
+  // create a blank array
+  this.books = this.retrieveLocalStorage() || [];
 }
 
 Library.prototype.addBook = function(book) {
   this.books.push(book);
+  this.updateLocalStorage();
 };
 
 // Mutates books
 Library.prototype.removeBook = function(id) {
   this.books.splice(id, 1);
+  this.updateLocalStorage();
 };
 
 // Does not mutate books
@@ -20,7 +24,8 @@ Library.prototype.removeBook = function(id) {
 //   });
 // }
 
-Library.prototype.render = function(books = this.books) {
+Library.prototype.render = function() {
+  const books = this.books;
   const bookButton = document.getElementById('add-book');
   this.addNewBookButtonListener(bookButton);
 
@@ -29,10 +34,14 @@ Library.prototype.render = function(books = this.books) {
   const root = document.getElementById('books');
   const docFrag = document.createDocumentFragment();
   books.forEach((book, index) => {
-    docFrag.appendChild(book.HTMLInfo(index));
+    const HTMLInfo = book.HTMLInfo(index);
+    HTMLInfo.appendChild(this.toggleReadButton(index));
+    HTMLInfo.appendChild(document.createElement('br'));
+    HTMLInfo.appendChild(this.deleteButton(index));
+
+    docFrag.appendChild(HTMLInfo);
   });
 
-  console.log(books);
   root.innerHTML = '';
   root.appendChild(docFrag);
 };
@@ -44,22 +53,23 @@ Library.prototype.addNewBookButtonListener = function(element) {
 Library.prototype.addBookSubmitListener = function() {
   const submit = document.getElementById('add-book-form');
 
-  const handleAddBookSubmit = e => this.handleAddBookSubmit(e);
+  // const handleAddBookSubmit = e => this.handleAddBookSubmit(e);
 
   // Remove the listener so you do not duplicate listeners
   // https://medium.com/@DavideRama/removeeventlistener-and-anonymous-functions-ab9dbabd3e7b
-  submit.addEventListener(
-    'submit',
-    function submitListener(e) {
-      handleAddBookSubmit(e);
+  // submit.addEventListener(
+  //   'submit',
+  //   function submitListener(e) {
+  //     handleAddBookSubmit(e);
 
-      // Because the page will rerender, the event listener mut be removed here
-      submit.removeEventListener('submit', submitListener, false);
-    },
-    false,
-  );
+  //     // Because the page will rerender, the event listener mut be removed here
+  //     submit.removeEventListener('submit', submitListener, false);
+  //   },
+  //   false,
+  // );
 
-  // submit.onsubmit = this.handleAddBookSubmit.bind(this);
+  // Alternative way to handle it
+  submit.onsubmit = this.handleAddBookSubmit.bind(this);
 };
 
 Library.prototype.handleAddBookSubmit = function(e) {
@@ -118,4 +128,64 @@ Library.prototype.formReset = function(form) {
 
   // Reset to the user has read the book
   form['has-read'].checked = true;
+};
+
+Library.prototype.toggleReadButton = function(id) {
+  const button = document.createElement('button');
+  let book = this.books[id];
+
+  if (book.read === true || book.read === 'true') {
+    button.innerText = 'Mark as unread';
+    button.addEventListener('click', () => this.markAsUnread(id), false);
+  } else {
+    button.innerText = 'Mark as read';
+    button.addEventListener('click', () => this.markAsRead(id), false);
+  }
+
+  return button;
+};
+
+Library.prototype.markAsUnread = function(id) {
+  this.books[id].read = false;
+  this.updateLocalStorage();
+  this.render();
+};
+
+Library.prototype.markAsRead = function(id) {
+  this.books[id].read = true;
+  this.updateLocalStorage();
+  this.render();
+};
+
+Library.prototype.deleteButton = function(id) {
+  const button = document.createElement('button');
+  button.innerText = 'Delete';
+  button.addEventListener('click', () => this.handleBookDelete(id));
+
+  return button;
+};
+
+Library.prototype.handleBookDelete = function(id) {
+  this.removeBook(id);
+  this.render();
+};
+
+Library.prototype.retrieveLocalStorage = function() {
+  try {
+    let books = JSON.parse(localStorage.getItem('books'));
+    books = books.map(b => Object.create(Book.prototype, b.values()));
+    console.log(books);
+    return books;
+  } catch (e) {
+    console.log('unable to retrieve from localStorage');
+  }
+};
+
+Library.prototype.updateLocalStorage = function() {
+  // Check that you can write to localStorage
+  try {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  } catch (e) {
+    console.log('unable to write to localStorage');
+  }
 };
